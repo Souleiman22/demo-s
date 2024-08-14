b3d03f50dd1fdc580eec365bdf90c139bb8174c8
Souleiman22 <97091184+Souleiman22@users.noreply.github.com>
2024-08-14 13:32:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("show") === -1) {
+      x.className += " show";
+    } else {
+      x.className = x.className.replace(" show", "");
+    }
+  }
+  
+<script>
+  function openWhatsApp() {
+    window.open('https://wa.me/yourphonenumber', '_blank');
+  }
+</script>
